4c7215ff5606b7650728ed415ae75541a28447c8
christopher.mbarapa.pro@gmail.com <test@debian>
2021-11-16 17:10:03 +0100

    fichier crée par Mbarapa dans projet Muwanga

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo'bonjour de Muwanga'
+        echo "ceci est une erreur de Mbarapa qui a remplacé une ligne";
         ?>
     </body>
 </html>
